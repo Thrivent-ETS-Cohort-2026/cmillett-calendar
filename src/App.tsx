674d2441a09e265b21f7031128fcbce3ad3cf54e
Calvin Millett <CMillettC@Yorksolutions.net>
2026-09-15 11:50:57 -0500

    fixed bug in 'App.tsx' to verify workflows success.

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ export default function App() {
     currentUser: currentUser
   }
 
-  functio displayDashboard() {
+  function displayDashboard() {
     if (currentUser) {
       return (
         <>
